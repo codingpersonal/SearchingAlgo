e47ee89a057c6a8e554e5816a2c7d68f50b0dd43
aggarwalsomya <aggarwal.somya@gmail.com>
2016-01-05 17:02:19 -0800

    Searching Algo

---
+++ src/BinarySearch.java
@@ -0,0 +1,31 @@
+
+public class BinarySearch {
+
+	public static void main(String[] args) {
+		int arr[] = {1,2,3,4,5,6,7,8,9};
+		int beg = 0;
+		int end = arr.length - 1;
+		int item = 10;
+		boolean flag = false;
+		
+		while(beg <= end) {
+			int mid = (beg + end) /2;
+			if(item == arr[mid]) {
+				System.out.println("Item found in the list");
+				flag = true;
+				break;
+			}
+			else if(item > arr[mid]) {
+				beg = mid + 1;
+			}
+			else {
+				end = mid - 1;
+			}
+		}
+		
+		if(!flag) {
+			System.out.println("Element is not present in the array");
+		}
+	}
+
+}
